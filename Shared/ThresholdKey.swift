effd7386a906c7c8ca9467285265909eaa22b3a3
metalurgical <97008724+metalurgical@users.noreply.github.com>
2022-10-26 20:36:12 +0200

    allow nullable parameters for ffi methods.

--- Shared/ThresholdKey.swift
+++ Shared/ThresholdKey.swift
@@ -14,12 +14,21 @@ final class ThresholdKey {
         self.pointer = pointer
     }
     
-    init(private_key: String, metadata: OpaquePointer, shares: OpaquePointer, storage_layer: StorageLayer, service_provider: ServiceProvider, local_matadata_transitions: OpaquePointer, last_fetch_cloud_metadata: OpaquePointer, enable_logging: Bool, manual_sync: Bool) throws {
+    init(private_key: String, metadata: OpaquePointer, shares: OpaquePointer, storage_layer: StorageLayer, service_provider: ServiceProvider?, local_matadata_transitions: OpaquePointer, last_fetch_cloud_metadata: OpaquePointer, enable_logging: Bool, manual_sync: Bool) throws {
         var errorCode: Int32 = -1
-        let keyPointer = UnsafeMutablePointer<Int8>(mutating: (private_key as NSString).utf8String)
         
+        var keyPointer = UnsafeMutablePointer<Int8>(nil)
+        if !private_key.isEmpty {
+            keyPointer = UnsafeMutablePointer<Int8>(mutating: (private_key as NSString).utf8String)
+        }
+        
+        var providerPointer = OpaquePointer(UnsafeMutablePointer<Int8>(nil))
+        if case .some(let provider) = service_provider {
+            providerPointer = provider.pointer
+        }
+
         let result = withUnsafeMutablePointer(to: &errorCode, { error in
-            threshold_key(keyPointer, metadata, shares, storage_layer.pointer, service_provider.pointer, local_matadata_transitions, last_fetch_cloud_metadata, enable_logging, manual_sync, error)
+            threshold_key(keyPointer, metadata, shares, storage_layer.pointer, providerPointer, local_matadata_transitions, last_fetch_cloud_metadata, enable_logging, manual_sync, error)
                 })
         guard errorCode == 0 else {
             throw RuntimeError("Error in ThresholdKey")
@@ -27,12 +36,21 @@ final class ThresholdKey {
         pointer = result!
     }
     
-    public func initialize(import_share: String, input: OpaquePointer, never_initialize_new_key: Bool, service_provider: ServiceProvider, include_local_metadata_transitions: Bool, curve_n: String) throws -> KeyResult
+    public func initialize(import_share: String, input: OpaquePointer, never_initialize_new_key: Bool, service_provider: ServiceProvider?, include_local_metadata_transitions: Bool, curve_n: String) throws -> KeyResult
     {
         var errorCode: Int32 = -1
-        let sharePointer = UnsafeMutablePointer<Int8>(mutating: (import_share as NSString).utf8String)
+        var sharePointer = UnsafeMutablePointer<Int8>(nil)
+        if !import_share.isEmpty {
+            sharePointer = UnsafeMutablePointer<Int8>(mutating: (import_share as NSString).utf8String)
+        }
+        
+        var providerPointer = OpaquePointer(UnsafeMutablePointer<Int8>(nil))
+        if case .some(let provider) = service_provider {
+            providerPointer = provider.pointer
+        }
+        
         let curvePointer = UnsafeMutablePointer<Int8>(mutating: (curve_n as NSString).utf8String)
-        let result = withUnsafeMutablePointer(to: &errorCode, { error in threshold_key_initialize(pointer, sharePointer, input, never_initialize_new_key, service_provider.pointer, include_local_metadata_transitions, curvePointer, error)})
+        let result = withUnsafeMutablePointer(to: &errorCode, { error in threshold_key_initialize(pointer, sharePointer, input, never_initialize_new_key, providerPointer, include_local_metadata_transitions, curvePointer, error)})
         guard errorCode == 0 else {
             throw RuntimeError("Error in ThresholdKey Initialize")
             }
