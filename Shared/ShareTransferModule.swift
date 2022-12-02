01ca75b2d9c8ab436b2a5ed4ac47e763e3f67a6b
metalurgical <97008724+metalurgical@users.noreply.github.com>
2022-12-02 05:30:56 +0200

    Revert "fix: share transfer test"

--- Shared/ShareTransferModule.swift
+++ Shared/ShareTransferModule.swift
@@ -18,7 +18,7 @@ final class ShareTransferModule {
             share_transfer_request_new_share(threshold_key.pointer, agentPointer, indexesPointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, request share. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, generate_new_share")
             }
         let string = String.init(cString: result!)
         string_destroy(result)
@@ -35,23 +35,22 @@ final class ShareTransferModule {
             share_transfer_add_custom_info_to_request(threshold_key.pointer, encPointer, customPointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, add custom info to request. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, input_share")
             }
     }
     
-    static func look_for_request(threshold_key: ThresholdKey) throws -> [String]
+    static func look_for_request(threshold_key: ThresholdKey) throws -> String
     {
         var errorCode: Int32 = -1
         let result = withUnsafeMutablePointer(to: &errorCode, { error in
             share_transfer_look_for_request(threshold_key.pointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, lookup for request. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         let string = String.init(cString: result!)
-        let indicator_array = try! JSONSerialization.jsonObject(with: string.data(using: String.Encoding.utf8)!, options: .allowFragments) as! [String]
         string_destroy(result)
-        return indicator_array
+        return string
     }
     
     static func approve_request(threshold_key: ThresholdKey, enc_pub_key_x: String, share_store: ShareStore, curve_n: String) throws
@@ -63,21 +62,21 @@ final class ShareTransferModule {
             share_transfer_approve_request(threshold_key.pointer, encPointer, share_store.pointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, change_question_and_answer. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
     }
     
-    static func approve_request_with_share_index(threshold_key: ThresholdKey, enc_pub_key_x: String, share_index: String, curve_n: String) throws
+    static func approve_request_with_share_indexes(threshold_key: ThresholdKey, enc_pub_key_x: String, share_indexes: String, curve_n: String) throws
     {
         var errorCode: Int32 = -1
         let curvePointer = UnsafeMutablePointer<Int8>(mutating: (curve_n as NSString).utf8String)
         let encPointer = UnsafeMutablePointer<Int8>(mutating: (enc_pub_key_x as NSString).utf8String)
-        let indexesPointer = UnsafeMutablePointer<Int8>(mutating: (share_index as NSString).utf8String)
+        let indexesPointer = UnsafeMutablePointer<Int8>(mutating: (enc_pub_key_x as NSString).utf8String)
         withUnsafeMutablePointer(to: &errorCode, { error in
             share_transfer_approve_request_with_share_indexes(threshold_key.pointer, encPointer, indexesPointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, approve request with share index. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
     }
     
@@ -88,7 +87,7 @@ final class ShareTransferModule {
             share_transfer_get_store(threshold_key.pointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, get store. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         return ShareTransferStore.init(pointer: result!)
     }
@@ -101,7 +100,7 @@ final class ShareTransferModule {
             share_transfer_set_store(threshold_key.pointer, store.pointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, set store. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         return result
     }
@@ -115,7 +114,7 @@ final class ShareTransferModule {
             share_transfer_delete_store(threshold_key.pointer, encPointer, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, delete store. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         return result
     }
@@ -127,7 +126,7 @@ final class ShareTransferModule {
             share_transfer_get_current_encryption_key(threshold_key.pointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, get current encryption key. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         let string = String.init(cString: result!)
         string_destroy(result)
@@ -144,7 +143,7 @@ final class ShareTransferModule {
             share_transfer_request_status_check(threshold_key.pointer, encPointer, delete_request_on_completion, curvePointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, request status check. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
         return ShareStore.init(pointer: result!)
     }
@@ -155,7 +154,7 @@ final class ShareTransferModule {
             share_transfer_cleanup_request(threshold_key.pointer, error)
                 })
         guard errorCode == 0 else {
-            throw RuntimeError("Error in ShareTransferModule, cleanup request. Error Code: \(errorCode)")
+            throw RuntimeError("Error in SecurityQuestionModule, change_question_and_answer")
             }
     }
 }
