5373b55d2a5ff6b2b402dcb9e0af6d3b4030a897
Guru Ramu <guru@tor.us>
2022-12-12 14:57:31 +0530

    feat: generate share test

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -9,6 +9,44 @@ import XCTest
 import tkey_ios
 
 class ThresholdKey_Test : XCTestCase {
+
+    func testGenerateDeleteShare() {
+        let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
+        let curve_n = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
+        let key1 = try! PrivateKey.generate(curve_n: curve_n)
+        let service_provider = try! ServiceProvider(enable_logging: true, postbox_key: key1.hex, curve_n: curve_n)
+        let threshold_key = try! ThresholdKey(
+            storage_layer: storage_layer,
+            service_provider: service_provider,
+            enable_logging: true,
+            manual_sync: true)
+
+        let _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
+        let key_details = try! threshold_key.get_key_details();
+        XCTAssertEqual(key_details.total_shares, 2);
+        
+        let new_share = try! threshold_key.generate_new_share(curve_n: curve_n);
+        let share_index = new_share.hex;
+        
+        let key_details_2 = try! threshold_key.get_key_details();
+        XCTAssertEqual(key_details_2.total_shares, 3);
+        
+        let output_share = try! threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n);
+        XCTAssertNotNil(output_share);
+        
+        try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n);
+        let key_details_3 = try! threshold_key.get_key_details();
+        XCTAssertEqual(key_details_3.total_shares, 2);
+        
+        do {
+            let _ = try threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n);
+        } catch let error as RuntimeError {
+            XCTAssertEqual(error.message, "Error in ThresholdKey generate_new_share");
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     func testThresholdInputOutputShare() throws {
         let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
         let version = try! library_version()
@@ -153,21 +191,4 @@ class ThresholdKey_Test : XCTestCase {
         assert(key_reconstruction_details.key == key_reconstruction_details_2.key, "Share transfer fail")
     }
     
-    func test_get_metadata() {
-        let curve_n = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
-        let key1 = try! PrivateKey.generate(curve_n: curve_n)
-        let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
-        let service_provider = try! ServiceProvider(enable_logging: true, postbox_key: key1.hex, curve_n: curve_n)
-        let threshold_key = try! ThresholdKey(
-            storage_layer: storage_layer,
-            service_provider: service_provider,
-            enable_logging: true,
-            manual_sync: true)
-        let _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
-        let _ = try! threshold_key.reconstruct(curve_n: curve_n)
-        let metadata = try! threshold_key.get_metadata()
-        let json = try! metadata.export()
-        assert(json.lengthOfBytes(using: .utf8) > 0)
-        let new_metadata = try! Metadata.init(json: json)
-    }
 }
