cbbfd2ec921f57de2335ddec545e93128c2fd3d2
ieow <cwlee0@gmail.com>
2022-12-13 15:42:26 +0800

    fix: xcframework lib

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -21,27 +21,27 @@ class ThresholdKey_Test: XCTestCase {
             enable_logging: true,
             manual_sync: true)
 
-        let _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
-        let key_details = try! threshold_key.get_key_details();
-        XCTAssertEqual(key_details.total_shares, 2);
+        _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
+        let key_details = try! threshold_key.get_key_details()
+        XCTAssertEqual(key_details.total_shares, 2)
 
-        let new_share = try! threshold_key.generate_new_share(curve_n: curve_n);
-        let share_index = new_share.hex;
+        let new_share = try! threshold_key.generate_new_share(curve_n: curve_n)
+        let share_index = new_share.hex
 
-        let key_details_2 = try! threshold_key.get_key_details();
-        XCTAssertEqual(key_details_2.total_shares, 3);
+        let key_details_2 = try! threshold_key.get_key_details()
+        XCTAssertEqual(key_details_2.total_shares, 3)
 
-        let output_share = try! threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n);
-        XCTAssertNotNil(output_share);
+        let output_share = try! threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
+        XCTAssertNotNil(output_share)
 
-        try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n);
-        let key_details_3 = try! threshold_key.get_key_details();
-        XCTAssertEqual(key_details_3.total_shares, 2);
+        try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n)
+        let key_details_3 = try! threshold_key.get_key_details()
+        XCTAssertEqual(key_details_3.total_shares, 2)
 
         do {
-            let _ = try threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n);
+            _ = try threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
         } catch let error as RuntimeError {
-            XCTAssertEqual(error.message, "Error in ThresholdKey generate_new_share");
+            XCTAssertEqual(error.message, "Error in ThresholdKey generate_new_share")
         } catch let error {
             print(error.localizedDescription)
         }
@@ -74,7 +74,7 @@ class ThresholdKey_Test: XCTestCase {
 
         _ = try! threshold_key2.initialize(never_initialize_new_key: true, include_local_metadata_transitions: false, curve_n: curve_n)
 
-        try! threshold_key2.input_share(share: shareOut, shareType: nil,       curve_n: curve_n)
+        try! threshold_key2.input_share(share: shareOut, shareType: nil,        curve_n: curve_n)
 
         let key2_reconstruction_details = try! threshold_key2.reconstruct(curve_n: curve_n)
         assert( key_reconstruction_details.key ==
@@ -93,55 +93,53 @@ class ThresholdKey_Test: XCTestCase {
             enable_logging: true,
             manual_sync: true)
 
-        let _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
-        let key_reconstruction_details = try! threshold_key.reconstruct(curve_n: curve_n);
+        _ = try! threshold_key.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false, curve_n: curve_n)
+        let key_reconstruction_details = try! threshold_key.reconstruct(curve_n: curve_n)
 
-        let question = "favorite marvel character";
-        let answer = "iron man";
-        let answer_2 = "captain america";
+        let question = "favorite marvel character"
+        let answer = "iron man"
+        let answer_2 = "captain america"
 
         // generate new security share
-        let new_share = try! SecurityQuestionModule.generate_new_share(threshold_key: threshold_key, questions: question, answer: answer, curve_n: curve_n);
-        let share_index = new_share.hex;
+        let new_share = try! SecurityQuestionModule.generate_new_share(threshold_key: threshold_key, questions: question, answer: answer, curve_n: curve_n)
+        let share_index = new_share.hex
 
-        let sq_question = try! SecurityQuestionModule.get_questions(threshold_key: threshold_key);
-        XCTAssertEqual(sq_question, question);
+        let sq_question = try! SecurityQuestionModule.get_questions(threshold_key: threshold_key)
+        XCTAssertEqual(sq_question, question)
 
-        let security_input_share: Bool = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n);
-        XCTAssertEqual(security_input_share, true);
+        let security_input_share: Bool = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
+        XCTAssertEqual(security_input_share, true)
 
         do {
-            let _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: "ant man", curve_n: curve_n);
+            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: "ant man", curve_n: curve_n)
         } catch let error as RuntimeError {
             XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
         }
 
         // change answer for already existing question
-        let change_answer_result = try! SecurityQuestionModule.change_question_and_answer(threshold_key: threshold_key, questions: question, answer: answer_2, curve_n: curve_n);
-        XCTAssertEqual(change_answer_result, true);
+        let change_answer_result = try! SecurityQuestionModule.change_question_and_answer(threshold_key: threshold_key, questions: question, answer: answer_2, curve_n: curve_n)
+        XCTAssertEqual(change_answer_result, true)
 
         do {
-            let _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n);
+            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
         } catch let error as RuntimeError {
             XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
         }
 
-        let security_input_share_2 = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer_2, curve_n: curve_n);
-        XCTAssertEqual(security_input_share_2, true);
+        let security_input_share_2 = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer_2, curve_n: curve_n)
+        XCTAssertEqual(security_input_share_2, true)
 
-
-
-        let get_answer = try! SecurityQuestionModule.get_answer(threshold_key: threshold_key);
-        XCTAssertEqual(get_answer, answer_2);
+        let get_answer = try! SecurityQuestionModule.get_answer(threshold_key: threshold_key)
+        XCTAssertEqual(get_answer, answer_2)
 
         let key_reconstruction_details_2 = try! threshold_key.reconstruct(curve_n: curve_n)
         assert(key_reconstruction_details.key == key_reconstruction_details_2.key, "security question fail")
 
         // delete newly security share
-        try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n);
+        try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n)
 
         do {
-            let _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n);
+            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
         } catch let error as RuntimeError {
             XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
         }
