ecb8f209e258afb62c251e88369a4cfc28529f7d
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-01-03 07:56:58 +0200

    refactor: cleanup

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -10,6 +10,10 @@ import tkey_ios
 
 class ThresholdKey_Test: XCTestCase {
 
+    func testLibraryVersion() {
+        _ = try! library_version()
+    }
+
     func testGenerateDeleteShare() {
         let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
         let curve_n = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
@@ -31,25 +35,20 @@ class ThresholdKey_Test: XCTestCase {
         let key_details_2 = try! threshold_key.get_key_details()
         XCTAssertEqual(key_details_2.total_shares, 3)
 
-        let output_share = try! threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
-        XCTAssertNotNil(output_share)
+        _ = try! threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
 
         try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n)
         let key_details_3 = try! threshold_key.get_key_details()
         XCTAssertEqual(key_details_3.total_shares, 2)
 
-        do {
-            _ = try threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
-        } catch let error as RuntimeError {
-            XCTAssertEqual(error.message, "Error in ThresholdKey generate_new_share")
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        XCTAssertThrowsError(
+            try threshold_key.output_share(shareIndex: share_index, shareType: nil, curve_n: curve_n)
+        )
+
     }
 
-    func testThresholdInputOutputShare() throws {
+    func testThresholdInputOutputShare() {
         let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
-        let version = try! library_version()
         let curve_n = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
         let key1 = try! PrivateKey.generate(curve_n: curve_n)
         let service_provider = try! ServiceProvider(enable_logging: true, postbox_key: key1.hex, curve_n: curve_n)
@@ -74,15 +73,13 @@ class ThresholdKey_Test: XCTestCase {
 
         _ = try! threshold_key2.initialize(never_initialize_new_key: true, include_local_metadata_transitions: false, curve_n: curve_n)
 
-        try! threshold_key2.input_share(share: shareOut, shareType: nil,           curve_n: curve_n)
+        try! threshold_key2.input_share(share: shareOut, shareType: nil, curve_n: curve_n)
 
         let key2_reconstruction_details = try! threshold_key2.reconstruct(curve_n: curve_n)
-        assert( key_reconstruction_details.key ==
-        key2_reconstruction_details.key, "key should be same")
-        debugPrint(version)
+        XCTAssertEqual( key_reconstruction_details.key, key2_reconstruction_details.key)
     }
 
-    func testSecurityQuestionModule() throws {
+    func testSecurityQuestionModule() {
         let storage_layer = try! StorageLayer(enable_logging: true, host_url: "https://metadata.tor.us", server_time_offset: 2)
         let curve_n = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
         let key1 = try! PrivateKey.generate(curve_n: curve_n)
@@ -110,21 +107,14 @@ class ThresholdKey_Test: XCTestCase {
         let security_input_share: Bool = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
         XCTAssertEqual(security_input_share, true)
 
-        do {
-            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: "ant man", curve_n: curve_n)
-        } catch let error as RuntimeError {
-            XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
-        }
+       XCTAssertThrowsError(try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: "ant man", curve_n: curve_n)
+            )
 
         // change answer for already existing question
         let change_answer_result = try! SecurityQuestionModule.change_question_and_answer(threshold_key: threshold_key, questions: question, answer: answer_2, curve_n: curve_n)
         XCTAssertEqual(change_answer_result, true)
 
-        do {
-            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
-        } catch let error as RuntimeError {
-            XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
-        }
+        XCTAssertThrowsError(try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n))
 
         let security_input_share_2 = try! SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer_2, curve_n: curve_n)
         XCTAssertEqual(security_input_share_2, true)
@@ -133,16 +123,12 @@ class ThresholdKey_Test: XCTestCase {
         XCTAssertEqual(get_answer, answer_2)
 
         let key_reconstruction_details_2 = try! threshold_key.reconstruct(curve_n: curve_n)
-        assert(key_reconstruction_details.key == key_reconstruction_details_2.key, "security question fail")
+        XCTAssertEqual(key_reconstruction_details.key, key_reconstruction_details_2.key)
 
         // delete newly security share
         try! threshold_key.delete_share(share_index: share_index, curve_n: curve_n)
 
-        do {
-            _ = try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n)
-        } catch let error as RuntimeError {
-            XCTAssertEqual(error.message, "Error in SecurityQuestionModule, input_share")
-        }
+        XCTAssertThrowsError(try SecurityQuestionModule.input_share(threshold_key: threshold_key, answer: answer, curve_n: curve_n))
     }
 
     func testThresholdShareTransfer () {
@@ -180,7 +166,7 @@ class ThresholdKey_Test: XCTestCase {
 
         let key_reconstruction_details_2 = try! threshold_key2.reconstruct(curve_n: curve_n)
 
-        assert(key_reconstruction_details.key == key_reconstruction_details_2.key, "Share transfer fail")
+        XCTAssertEqual(key_reconstruction_details.key, key_reconstruction_details_2.key)
     }
 
     func testPrivateKeyModule() {
@@ -199,30 +185,28 @@ class ThresholdKey_Test: XCTestCase {
         _ = try! threshold_key.reconstruct(curve_n: curve_n)
 
         let result = try! PrivateKeysModule.get_private_key_accounts(threshold_key: threshold_key)
-        assert(result.count == 0)
+        XCTAssertEqual(result.count, 0)
 
         let key_module = try! PrivateKey.generate(curve_n: curve_n)
         let key_module2 = try! PrivateKey.generate(curve_n: curve_n)
-//        Done setup
-
-//        Try set and get privatekey from privatekey module
+        // Done setup
+        // Try set and get privatekey from privatekey module
         _ = try! PrivateKeysModule.set_private_key(threshold_key: threshold_key, key: key_module.hex, format: "secp256k1n", curve_n: curve_n)
         let result_1 = try! PrivateKeysModule.get_private_key_accounts(threshold_key: threshold_key)
-        assert(result_1 == [key_module.hex] )
+        XCTAssertEqual(result_1, [key_module.hex] )
 
-//        Try set 2nd privatekey
+        // Try set 2nd privatekey
         _ = try! PrivateKeysModule.set_private_key(threshold_key: threshold_key, key: key_module2.hex, format: "secp256k1n", curve_n: curve_n)
         let result_2 = try! PrivateKeysModule.get_private_key_accounts(threshold_key: threshold_key)
-        assert( result_2 == [key_module.hex, key_module2.hex] )
+        XCTAssertEqual(result_2, [key_module.hex, key_module2.hex])
 
-//        Try set privateKey module with nil key
+        // Try set privateKey module with nil key
         _ = try! PrivateKeysModule.set_private_key(threshold_key: threshold_key, key: nil, format: "secp256k1n", curve_n: curve_n)
         let result_3 = try! PrivateKeysModule.get_private_key_accounts(threshold_key: threshold_key)
-        assert(result_3.count == 3 )
+        XCTAssertEqual(result_3.count, 3)
 
-        //        try PrivateKeysModule.remove_private_key()
-
-//        Reconstruct on second instance and check value ?
+        // try PrivateKeysModule.remove_private_key()
+        // Reconstruct on second instance and check value ?
 
     }
 
@@ -245,37 +229,29 @@ class ThresholdKey_Test: XCTestCase {
 
 //        Check the seedphrase module is empty
         let seedResult = try! SeedPhraseModule.get_seed_phrases(threshold_key: threshold_key)
-        assert(seedResult.count == 0 )
+        XCTAssertEqual(seedResult.count, 0 )
 
 //        set and get seedphrases
         try! SeedPhraseModule.set_seed_phrase(threshold_key: threshold_key, format: "HD Key Tree", phrase: seedPhraseToSet, number_of_wallets: 0, curve_n: curve_n)
         let seedResult_2 = try! SeedPhraseModule.get_seed_phrases(threshold_key: threshold_key)
-        assert(seedResult_2[0].seedPhrase == seedPhraseToSet )
+        XCTAssertEqual(seedResult_2[0].seedPhrase, seedPhraseToSet )
 
 //        Try delete unknown seedphrase - expect fail
-        do {
-            try SeedPhraseModule.delete_seedphrase(threshold_key: threshold_key, phrase: seedPhraseToSet2, curve_n: curve_n)
-            assert(false)
-        } catch {
-            debugPrint("expected delete failed")
-        }
+        XCTAssertThrowsError(try SeedPhraseModule.delete_seedphrase(threshold_key: threshold_key, phrase: seedPhraseToSet2, curve_n: curve_n))
 
 //        Try to set and get 2nd seedphrases
         try! SeedPhraseModule.set_seed_phrase(threshold_key: threshold_key, format: "HD Key Tree", phrase: seedPhraseToSet2, number_of_wallets: 0, curve_n: curve_n)
         let seedResult_3 = try! SeedPhraseModule.get_seed_phrases(threshold_key: threshold_key)
-        assert(seedResult_3[0].seedPhrase == seedPhraseToSet )
-        assert(seedResult_3[1].seedPhrase == seedPhraseToSet2 )
+        XCTAssertEqual(seedResult_3[0].seedPhrase, seedPhraseToSet )
+        XCTAssertEqual(seedResult_3[1].seedPhrase, seedPhraseToSet2 )
 
 //        Try set seedphrase with existing seed phrase
-        do {
-            try SeedPhraseModule.set_seed_phrase(threshold_key: threshold_key, format: "HD Key Tree", phrase: seedPhraseToSet2, number_of_wallets: 0, curve_n: curve_n)
-            assert(false, "unexpected set same seedphrase")
-        } catch {
-        }
+        XCTAssertThrowsError(try SeedPhraseModule.set_seed_phrase(threshold_key: threshold_key, format: "HD Key Tree", phrase: seedPhraseToSet2, number_of_wallets: 0, curve_n: curve_n))
+
 //        Try set seedphrase with nil
         try! SeedPhraseModule.set_seed_phrase(threshold_key: threshold_key, format: "HD Key Tree", phrase: nil, number_of_wallets: 0, curve_n: curve_n)
         let seedResult_4 = try! SeedPhraseModule.get_seed_phrases(threshold_key: threshold_key)
-        assert(seedResult_4.count == 3 )
+        XCTAssertEqual(seedResult_4.count, 3 )
 
 //        Try reconstruct 2nd Tkey instance to check if seed phrase is persistance
     }
@@ -295,7 +271,7 @@ class ThresholdKey_Test: XCTestCase {
         _ = try! threshold_key.reconstruct(curve_n: curve_n)
         let metadata = try! threshold_key.get_metadata()
         let json = try! metadata.export()
-        assert(json.lengthOfBytes(using: .utf8) > 0)
+        XCTAssertGreaterThan(json.lengthOfBytes(using: .utf8), 0)
         _ = try! Metadata.init(json: json)
     }
 }
