38fbfbb4c970d89395a5f8c1767f5f821bc8b912
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-07-28 12:18:31 +0200

    add tss-client-swift

--- Shared/TssView.swift
+++ Shared/TssView.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import SwiftUI
+import BigInt
 import tkey_pkg
+import tss_client_swift
 
 struct TssView: View {
     @State var threshold_key: ThresholdKey!
     @State private var tss_modules: [String: TssModule] = [:]
+    @State var showAlert: Bool = false
 
     var body: some View {
             Section(header: Text("Tss Module")) {
@@ -91,6 +94,37 @@ struct TssView: View {
                             // get factor key from keychain
                             // get tss share using factor key
                             // sign transaction using tss client
+                            let parties = 4
+                            let msg = "hello world"
+                            let msgHash = TSSHelpers.hashMessage(message: msg)
+                            let clientIndex = Int32(parties-1)
+                            let session = TSSHelpers.assembleFullSession(verifier: "", verifierId: "", tssTag: "", tssNonce: "", sessionNonce: "")
+                            let sigs: [String] = []
+                            let endpoints: [String?] = []
+                            let socketEndpoints: [String?] = []
+                            let partyIndexes: [Int32] = [0, 1, 2, 3]
+                            let share = BigInt(1)
+                            let userSharePublicKey = Data(BigInt(1).serialize().suffix(32))
+                            let dkgPub = Data(BigInt(1).serialize().suffix(32))
+                            let publicKey = try! TSSHelpers.getFinalTssPublicKey(dkgPubKey: dkgPub, userSharePubKey: userSharePublicKey, userTssIndex: BigInt(1))
+                            let coeffs = try! TSSHelpers.getServerCoeffiecients(participatingServerDKGIndexes: [BigInt(1)], userTssIndex: BigInt(1), serverIndex: BigInt(1))
+
+                            let client = try! TSSClient(session: session, index: clientIndex, parties: partyIndexes, endpoints: endpoints.map({ URL(string: $0 ?? "") }), tssSocketEndpoints: socketEndpoints.map({ URL(string: $0 ?? "") }), share: TSSHelpers.base64Share(share: share), pubKey: try TSSHelpers.base64PublicKey(pubKey: publicKey))
+                            while !client.checkConnected() {
+
+                            }
+
+                            let precompute = try! client.precompute(serverCoeffs: coeffs, signatures: sigs)
+                            while !(try! client.isReady()) {
+
+                            }
+                            let (s, r, v) = try! client.sign(message: msgHash, hashOnly: true, original_message: msg, precompute: precompute, signatures: sigs)
+                            try! client.cleanup(signatures: sigs)
+                            if TSSHelpers.verifySignature(msgHash: msgHash, s: s, r: r, v: v, pubKey: publicKey) {
+                                showAlert = true
+                            } else {
+                                exit(EXIT_FAILURE)
+                            }
                         }
                     }) { Text("sign with tagged tss share") }
                 }
