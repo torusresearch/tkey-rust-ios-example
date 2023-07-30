bd45e758a6038ea54fcf563966aba23cb9019161
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-07-30 07:46:12 +0200

    Update LoginModel.swift

--- Shared/LoginModel.swift
+++ Shared/LoginModel.swift
@@ -29,8 +29,7 @@ class LoginModel: ObservableObject {
                                          verifier: "google-lrc",
                                          redirectURL: "tdsdk://tdsdk/oauthCallback",
                                          browserRedirectURL: "https://scripts.toruswallet.io/redirect.html")
-            let tdsdk = CustomAuth( aggregateVerifierType: .singleLogin, aggregateVerifier: "google-lrc", subVerifierDetails: [sub], network: // .sapphire(.SAPPHIRE_DEVNET), enableOneKey: true)
-                .legacy(.TESTNET), enableOneKey: true)
+            let tdsdk = CustomAuth( aggregateVerifierType: .singleLogin, aggregateVerifier: "google-lrc", subVerifierDetails: [sub], network: .sapphire(.SAPPHIRE_DEVNET), enableOneKey: true)
             let data = try await tdsdk.triggerLogin()
             print(data)
 
