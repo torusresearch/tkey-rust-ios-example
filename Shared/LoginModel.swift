a77508b0c8234141aad38c3936aafa560d83d50f
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-03-08 10:15:32 +0200

    fix: update packages

--- Shared/LoginModel.swift
+++ Shared/LoginModel.swift
@@ -29,10 +29,10 @@ class LoginModel: ObservableObject {
                                          verifier: "google-lrc",
                                          redirectURL: "tdsdk://tdsdk/oauthCallback",
                                          browserRedirectURL: "https://scripts.toruswallet.io/redirect.html")
-            let tdsdk = CustomAuth(aggregateVerifierType: .singleLogin, aggregateVerifier: "google-lrc", subVerifierDetails: [sub], network: .TESTNET)
+            let tdsdk = CustomAuth(aggregateVerifierType: .singleLogin, aggregateVerifier: "google-lrc", subVerifierDetails: [sub], network: .legacy(.TESTNET))
             let data = try await tdsdk.triggerLogin()
             await MainActor.run(body: {
-                self.userData = data
+                self.userData = data.userInfo
                 loggedIn = true
             })
         }
